b692ede94b671a217f66a304f15703776731b4e6
Umang Giri <umanggiri103@gmail.com>
2026-04-02 13:19:38 +0545

    added video title, description and like count by fetching the api

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./PlayVideo.css";
-import video1 from "../../assets/video.mp4";
+
 import like from "../../assets/like.png";
 import dislike from "../../assets/dislike.png";
 import share from "../../assets/share.png";
 import save from "../../assets/save.png";
 import jack from "../../assets/jack.png";
 import user_profile from "../../assets/user_profile.jpg";
-const PlayVideo = () => {
+import { useState } from "react";
+import { API_KEY, value_converter } from "../../data";
+import moment from "moment";
+const PlayVideo = ({ videoId }) => {
+  const [apiData, setApiData] = useState(null);
+  const [];
+
+  const fetchVideoData = async () => {
+    //Fetching video details
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+    await fetch(videoDetails_url)
+      .then((response) => response.json())
+      .then((data) => setApiData(data.items[0]));
+  };
+
+  useEffect(() => {
+    fetchVideoData();
+  }, [videoId]);
+
   return (
     <div className="play-video">
-      <video src={video1} controls autoPlay muted></video>
-      <h3>Best Youtube channel to learn Web Development</h3>
+      {/*<video src={video1} controls autoPlay muted></video>*/}
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
+      <h3>{apiData ? apiData.snippet.title : "Title here"}</h3>
       <div className="play-video-info">
-        <p>1M views &bull; 2 days ago</p>
+        <p>
+          {apiData
+            ? `${value_converter(apiData.statistics.viewCount)} views • ${moment(
+                apiData.snippet.publishedAt,
+              ).fromNow()}`
+            : "Loading..."}
+        </p>
         <div>
           <span>
             <img src={like} alt="Like" />
-            125
+            {apiData ? value_converter(apiData.statistics.likeCount) : "0"}
           </span>
           <span>
-            <img src={dislike} alt="Dislike" />2
+            <img src={dislike} alt="Dislike" />
           </span>
           <span>
             <img src={share} alt="Share" />
@@ -36,20 +67,22 @@ const PlayVideo = () => {
       <div className="publisher">
         <img src={jack} alt="Publisher" />
         <div>
-          <p>GreatStack</p>
-          <span>1M subscribers</span>
+          <p>{apiData ? apiData.snippet.channelTitle : "Channel Name"}</p>
+          <span>{} subscribers</span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="vid-description">
-        <p>Channel that makes learning easier</p>
         <p>
-          Suscribe GreatStack to watch more videos on Web Development and
-          Programming. We make videos on React, JavaScript, Node.js, CSS, HTML
-          and more. We also make videos on competitive programming and DSA.
+          {apiData
+            ? apiData.snippet.description.slice(0, 250)
+            : "Description here"}
         </p>
         <hr />
-        <h4>130 comments</h4>
+        <h4>
+          {apiData ? value_converter(apiData.statistics.commentCount) : "102"}{" "}
+          comments
+        </h4>
         <div className="comment">
           <img src={user_profile} alt="User" />
           <div>
--- src/data.js
+++ src/data.js
@@ -1,1 +1,9 @@
 export const API_KEY = "AIzaSyDj3eIS1OwzTEfeRzZ6P4qyu3gI2rI5dpo";
+export const value_converter = (value) => {
+  if (value >= 1000000) {
+    return Math.floor(value / 1000000) + "M";
+  } else if (value >= 1000) {
+    return Math.floor(value / 1000) + "K";
+  }
+  return value;
+};
